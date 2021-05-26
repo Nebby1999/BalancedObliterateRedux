ff79877400c9df2797969bbfd1103bef6240ed13
BuildTools <unconfigured@null.spigotmc.org>
2021-05-26 15:24:35 -0400

    Add project files.

---
+++ ConfigLoader.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalancedObliterateRedux
+{
+    internal class ConfigLoader
+    {
+        internal static ConfigEntry<float> CoinsPerStageBeatenConfig { get; set; }
+        public static float CoinsPerStageBeaten => CoinsPerStageBeatenConfig.Value;
+        internal static ConfigEntry<bool> StagePenaltyConfig { get; set; }
+        public static bool StagePenalty => StagePenaltyConfig.Value;
+        internal static ConfigEntry<float> DrizzleMultiplierConfig { get; set; }
+        public static float DrizzleMultiplier => DrizzleMultiplierConfig.Value;
+        internal static ConfigEntry<float> RainstormMultiplierConfig { get; set; }
+        public static float RainstormMultiplier => RainstormMultiplierConfig.Value;
+        internal static ConfigEntry<float> MonsoonMultiplierConfig { get; set; }
+        public static float MonsoonMultiplier => MonsoonMultiplierConfig.Value;
+        internal static ConfigEntry<float> TyphoonMultiplierConfig { get; set; }
+        public static float TyphoonMultiplier => TyphoonMultiplierConfig.Value;
+        internal static ConfigEntry<bool> EnableFallBackConfig { get; set; }
+        public static bool EnableFallBack => EnableFallBackConfig.Value;
+
+        public static void Init(ConfigFile config)
+        {
+            CoinsPerStageBeatenConfig = config.Bind<float>("1 - General Settings", "Coins Per Stage Beaten", 0.25f, "How many extra coins you get per stage beaten past Stage 8\nThis number is the base number of the mathematical equation which dictates how many extra coins you get when obliterating.");
+            StagePenaltyConfig = config.Bind<bool>("1 - General Settings", "Coin Per Stage Starts Past Stage 5", true, "By default, the mod starts counting the Coins Per Stage Beaten once you first beat the boss of the Primordial Teleporter\nThis is to encourage players to go past stage 8 and obliterating later for bigger rewards.");
+
+            DrizzleMultiplierConfig = config.Bind<float>("2 - Difficulty Multipliers", "Drizzle Multiplier", 1.0f, "The multiplier applied to the amount of stages beaten.");
+            RainstormMultiplierConfig = config.Bind<float>("2 - Difficulty Multipliers", "Rainstorm Multiplier", 1.5f, "The multiplier applied to the amount of stages beaten.");
+            MonsoonMultiplierConfig = config.Bind<float>("2 - Difficulty Multipliers", "Monsoon Multiplier", 2.0f, "The multiplier applied to the amount of stages beaten.");
+            TyphoonMultiplierConfig = config.Bind<float>("2 - Difficulty Multipliers", "Typhoon Multiplier", 2.5f, "The multiplier applied to the amount of stages beaten.");
+        }
+    }
+}
---
+++ MainClass.cs
@@ -0,0 +1,127 @@
+﻿using BepInEx;
+using RoR2;
+using Starstorm2.Cores;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace BalancedObliterateRedux
+{
+    [BepInDependency("com.TeamMoonstorm.Starstorm2", BepInDependency.DependencyFlags.SoftDependency)]
+    [BepInPlugin("com.Nebby1999.BalancedObliterateRedux", "BalancedObliterateRedux", "1.0.1")]
+    public class MainClass : BaseUnityPlugin
+    {
+        internal static bool hasSS2 = false;
+        public void Awake()
+        {
+            if(BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.TeamMoonstorm.Starstorm2"))
+            {
+                hasSS2 = true;
+                Logger.LogMessage("Starstorm 2 Found!");
+            }
+            ConfigLoader.Init(Config);
+            Run.onServerGameOver += MainMethod;
+        }
+
+        public void OnDestroy()
+        {
+            Run.onServerGameOver -= MainMethod;
+        }
+
+        private void MainMethod(Run run, GameEndingDef gameEndingDef)
+        {
+            float stageReward;
+            if (ConfigLoader.StagePenalty)
+            {
+                stageReward = ConfigLoader.CoinsPerStageBeaten * ((run.stageClearCount - 5) * run.loopClearCount);
+            }
+            else
+            {
+                stageReward = ConfigLoader.CoinsPerStageBeaten * ((run.stageClearCount) * run.loopClearCount);
+            }
+            float difficultyMultiplier = GetDifficultyMultiplier(run.selectedDifficulty, hasSS2);
+            Logger.LogMessage("Stages cleared: " + run.stageClearCount);
+            Logger.LogMessage("Loops Cleared: " + run.loopClearCount);
+            Logger.LogMessage("Game Ending: " + gameEndingDef);
+            Logger.LogMessage("-----------REWARDS----------");
+            Logger.LogMessage("Reward for Stages Completed & Loops Completed: " + stageReward);
+            Logger.LogMessage("Difficulty Multiplier: " + difficultyMultiplier);
+            if (gameEndingDef == RoR2Content.GameEndings.StandardLoss)
+            {
+                Logger.LogMessage("Game Ending is Standard Loss, Aborting...");
+                return;
+            }
+            else if (gameEndingDef == RoR2Content.GameEndings.ObliterationEnding)
+            {
+                Logger.LogMessage("Game Ending is Obliteration, proceeding to calculate rewards...");
+                var LunarCoinAward = (stageReward * difficultyMultiplier);
+                Logger.LogMessage("Extra Lunar Coins to give: " + LunarCoinAward);
+                if (LunarCoinAward > 0)
+                {
+                    uint totalCoins = Convert.ToUInt32(Math.Ceiling(LunarCoinAward));
+                    for (int i = 0; i < NetworkUser.readOnlyInstancesList.Count; i++)
+                    {
+                        NetworkUser networkUser = NetworkUser.readOnlyInstancesList[i];
+                        if (networkUser && networkUser.isParticipating)
+                        {
+                            Logger.LogMessage("Awarding " + totalCoins + " to network user " + networkUser.GetNetworkPlayerName());
+                            networkUser.AwardLunarCoins(totalCoins);
+                        }
+                    }
+                }
+                else
+                {
+                    Logger.LogMessage("Extra Lunar Coins to Give is less than 0, Aborting extra coins...");
+                }
+            }
+            else
+            {
+                Logger.LogMessage("Game ending is not obliteration, Aborting...");
+                return;
+            }
+        }
+        private float GetDifficultyMultiplier(DifficultyIndex difficultyIndex, bool hasStarstorm2)
+        {
+            if(hasStarstorm2)
+            {
+                var multiplier = GetStarstormDificulty(difficultyIndex);
+                return multiplier;
+            }
+            else
+            {
+                if (difficultyIndex == DifficultyIndex.Easy)
+                {
+                    return ConfigLoader.DrizzleMultiplier;
+                }
+                else if (difficultyIndex == DifficultyIndex.Normal)
+                {
+                    return ConfigLoader.RainstormMultiplier;
+                }
+                else if (difficultyIndex == DifficultyIndex.Hard)
+                {
+                    return ConfigLoader.MonsoonMultiplier;
+                }
+            }
+            return ConfigLoader.RainstormMultiplier;
+        }
+        private float GetStarstormDificulty(DifficultyIndex difficultyIndex)
+        {
+            if (difficultyIndex == DifficultyIndex.Easy)
+            {
+                return ConfigLoader.DrizzleMultiplier;
+            }
+            else if (difficultyIndex == DifficultyIndex.Normal)
+            {
+                return ConfigLoader.RainstormMultiplier;
+            }
+            else if (difficultyIndex == DifficultyIndex.Hard)
+            {
+                return ConfigLoader.MonsoonMultiplier;
+            }
+            else if (difficultyIndex == TyphoonCore.diffIdxTyphoon)
+            {
+                return ConfigLoader.TyphoonMultiplier;
+            }
+            return ConfigLoader.RainstormMultiplier;
+        }
+    }
+}
