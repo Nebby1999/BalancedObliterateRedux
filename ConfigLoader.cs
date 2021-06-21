c303f41e2b751f0b1cadbde1454552188bf1a243
Nebby <nebby131999@gmail.com>
2021-06-21 16:32:40 -0400

    Updated to version 1.1.0

--- ConfigLoader.cs
+++ ConfigLoader.cs
@@ -9,6 +9,8 @@ namespace BalancedObliterateRedux
 {
     internal class ConfigLoader
     {
+        internal static ConfigEntry<string> CalculationMethodsConfig { get; set; }
+        public static string CalculationMethods => CalculationMethodsConfig.Value;
         internal static ConfigEntry<float> CoinsPerStageBeatenConfig { get; set; }
         public static float CoinsPerStageBeaten => CoinsPerStageBeatenConfig.Value;
         internal static ConfigEntry<bool> StagePenaltyConfig { get; set; }
@@ -26,13 +28,14 @@ namespace BalancedObliterateRedux
 
         public static void Init(ConfigFile config)
         {
-            CoinsPerStageBeatenConfig = config.Bind<float>("1 - General Settings", "Coins Per Stage Beaten", 0.25f, "How many extra coins you get per stage beaten past Stage 8\nThis number is the base number of the mathematical equation which dictates how many extra coins you get when obliterating.");
-            StagePenaltyConfig = config.Bind<bool>("1 - General Settings", "Coin Per Stage Starts Past Stage 5", true, "By default, the mod starts counting the Coins Per Stage Beaten once you first beat the boss of the Primordial Teleporter\nThis is to encourage players to go past stage 8 and obliterating later for bigger rewards.");
+            CalculationMethodsConfig = config.Bind<string>("1 - Calculation Method", "Calculation Methods", "Redux2.0", "What Type of Calculation method the mod uses.\nRedux2.0: Coin Calculation is based off using the Difficulty multiplier assigned in the DifficultyDef.\nRedux: Coin Calculation is based off the configurable multipliers in this config file. if a modded difficulty is not listed, it'll fall back to the rainstorm difficulty multiplier.");
+            CoinsPerStageBeatenConfig = config.Bind<float>("2 - General Settings", "Coins Per Stage Beaten", 0.25f, "How many extra coins you get per stage beaten \nThis number is the base number of the mathematical equation which dictates how many extra coins you get when obliterating.");
+            StagePenaltyConfig = config.Bind<bool>("2 - General Settings", "Coin Per Stage Starts Past Stage 5", true, "By default, the mod starts counting the Coins Per Stage Beaten once you first beat the boss of the Primordial Teleporter\nThis is to encourage players to go past stage 8 and obliterating later for bigger rewards.");
 
-            DrizzleMultiplierConfig = config.Bind<float>("2 - Difficulty Multipliers", "Drizzle Multiplier", 1.0f, "The multiplier applied to the amount of stages beaten.");
-            RainstormMultiplierConfig = config.Bind<float>("2 - Difficulty Multipliers", "Rainstorm Multiplier", 1.5f, "The multiplier applied to the amount of stages beaten.");
-            MonsoonMultiplierConfig = config.Bind<float>("2 - Difficulty Multipliers", "Monsoon Multiplier", 2.0f, "The multiplier applied to the amount of stages beaten.");
-            TyphoonMultiplierConfig = config.Bind<float>("2 - Difficulty Multipliers", "Typhoon Multiplier", 2.5f, "The multiplier applied to the amount of stages beaten.");
+            DrizzleMultiplierConfig = config.Bind<float>("3 - Difficulty Multipliers", "Drizzle Multiplier", 1.0f, "The multiplier applied to the amount of stages beaten.");
+            RainstormMultiplierConfig = config.Bind<float>("3 - Difficulty Multipliers", "Rainstorm Multiplier", 1.5f, "The multiplier applied to the amount of stages beaten.");
+            MonsoonMultiplierConfig = config.Bind<float>("3 - Difficulty Multipliers", "Monsoon Multiplier", 2.0f, "The multiplier applied to the amount of stages beaten.");
+            TyphoonMultiplierConfig = config.Bind<float>("3 - Difficulty Multipliers", "Typhoon Multiplier", 2.5f, "The multiplier applied to the amount of stages beaten.");
         }
     }
 }
--- MainClass.cs
+++ MainClass.cs
@@ -7,42 +7,90 @@ using System.Runtime.CompilerServices;
 namespace BalancedObliterateRedux
 {
     [BepInDependency("com.TeamMoonstorm.Starstorm2", BepInDependency.DependencyFlags.SoftDependency)]
-    [BepInPlugin("com.Nebby1999.BalancedObliterateRedux", "BalancedObliterateRedux", "1.0.1")]
+    [BepInPlugin("com.Nebby1999.BalancedObliterateRedux", "BalancedObliterateRedux", "1.1.0")]
     public class MainClass : BaseUnityPlugin
     {
+        internal float stageReward;
+        internal int stagePenalty;
         internal static bool hasSS2 = false;
         public void Awake()
         {
-            if(BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.TeamMoonstorm.Starstorm2"))
+            if (BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.TeamMoonstorm.Starstorm2"))
             {
                 hasSS2 = true;
                 Logger.LogMessage("Starstorm 2 Found!");
             }
             ConfigLoader.Init(Config);
-            Run.onServerGameOver += MainMethod;
-        }
-
-        public void OnDestroy()
-        {
-            Run.onServerGameOver -= MainMethod;
-        }
-
-        private void MainMethod(Run run, GameEndingDef gameEndingDef)
-        {
-            float stageReward;
             if (ConfigLoader.StagePenalty)
             {
-                stageReward = ConfigLoader.CoinsPerStageBeaten * ((run.stageClearCount - 5) * run.loopClearCount);
+                stagePenalty = 5;
+            }
+            if (ConfigLoader.CalculationMethods == "Redux2.0")
+            {
+                Run.onServerGameOver += Redux2Method;
+            }
+            else if (ConfigLoader.CalculationMethods == "Redux")
+            {
+                Run.onServerGameOver += ReduxMethod;
             }
             else
             {
-                stageReward = ConfigLoader.CoinsPerStageBeaten * ((run.stageClearCount) * run.loopClearCount);
+                Logger.LogWarning(ConfigLoader.CalculationMethodsConfig.Definition + " is not valid! balanced obliterate redux will not do changes.");
             }
-            float difficultyMultiplier = GetDifficultyMultiplier(run.selectedDifficulty, hasSS2);
-            Logger.LogMessage("Stages cleared: " + run.stageClearCount);
+        }
+
+        private void Redux2Method(Run run, GameEndingDef gameEndingDef)
+        {
+            float difficultyMultiplier = DifficultyCatalog.GetDifficultyDef(run.selectedDifficulty).scalingValue;
+            Logger.LogMessage("Stages Cleared: " + run.stageClearCount);
             Logger.LogMessage("Loops Cleared: " + run.loopClearCount);
-            Logger.LogMessage("Game Ending: " + gameEndingDef);
-            Logger.LogMessage("-----------REWARDS----------");
+            Logger.LogMessage("This run's Game Ending: " + gameEndingDef.cachedName);
+            Logger.LogMessage("----------REWARDS----------");
+            stageReward = ConfigLoader.CoinsPerStageBeaten * ((run.stageClearCount - stagePenalty) * run.loopClearCount);
+            Logger.LogMessage("reward for Stages Completed & Loops Completed: " + stageReward);
+            Logger.LogMessage("Difficulty Multiplier: " + difficultyMultiplier);
+            if (gameEndingDef == RoR2Content.GameEndings.StandardLoss)
+            {
+                Logger.LogMessage("Game Ending is Standard Loss, Aborting...");
+                return;
+            }
+            else if(gameEndingDef == RoR2Content.GameEndings.ObliterationEnding)
+            {
+                Logger.LogMessage("Game Ending is Obliteration, proceeding to calculate rewards...");
+                var LunarCoinAward = (stageReward * difficultyMultiplier);
+                Logger.LogMessage("Extra Lunar Coins to Give: " + LunarCoinAward);
+                if(LunarCoinAward > 0)
+                {
+                    uint totalCoins = Convert.ToUInt32(Math.Ceiling(LunarCoinAward));
+                    for (int i = 0; i < NetworkUser.readOnlyInstancesList.Count; i++)
+                    {
+                        NetworkUser networkUser = NetworkUser.readOnlyInstancesList[i];
+                        if(networkUser && networkUser.isParticipating)
+                        {
+                            Logger.LogMessage("Awarding " + totalCoins + " to network user " + networkUser.GetNetworkPlayerName().nameOverride);
+                            networkUser.AwardLunarCoins(totalCoins);
+                        }
+                    }
+                }
+                else
+                {
+                    Logger.LogMessage("Extra Lunar Coins to Give is less than 0, Aborting extra coins...");
+                }
+            }
+            else
+            {
+                Logger.LogMessage("Game ending is not obliteration, Aborting...");
+                return;
+            }
+        }
+        private void ReduxMethod(Run run, GameEndingDef gameEndingDef)
+        {
+            float difficultyMultiplier = GetDifficultyMultiplier(run.selectedDifficulty, hasSS2);
+            Logger.LogMessage("Stages Cleared: " + run.stageClearCount);
+            Logger.LogMessage("Loops Cleared: " + run.loopClearCount);
+            Logger.LogMessage("This run's Game Ending: " + gameEndingDef);
+            Logger.LogMessage("----------REWARDS----------");
+            stageReward = ConfigLoader.CoinsPerStageBeaten * ((run.stageClearCount - stagePenalty) * run.loopClearCount);
             Logger.LogMessage("Reward for Stages Completed & Loops Completed: " + stageReward);
             Logger.LogMessage("Difficulty Multiplier: " + difficultyMultiplier);
             if (gameEndingDef == RoR2Content.GameEndings.StandardLoss)
@@ -63,7 +111,7 @@ namespace BalancedObliterateRedux
                         NetworkUser networkUser = NetworkUser.readOnlyInstancesList[i];
                         if (networkUser && networkUser.isParticipating)
                         {
-                            Logger.LogMessage("Awarding " + totalCoins + " to network user " + networkUser.GetNetworkPlayerName());
+                            Logger.LogMessage("Awarding " + totalCoins + " to network user " + networkUser.GetNetworkPlayerName().nameOverride);
                             networkUser.AwardLunarCoins(totalCoins);
                         }
                     }
@@ -77,6 +125,18 @@ namespace BalancedObliterateRedux
             {
                 Logger.LogMessage("Game ending is not obliteration, Aborting...");
                 return;
+            }
+        }
+
+        public void OnDestroy()
+        {
+            if (ConfigLoader.CalculationMethods == "Redux2.0")
+            {
+                Run.onServerGameOver -= Redux2Method;
+            }
+            else if (ConfigLoader.CalculationMethods == "Redux")
+            {
+                Run.onServerGameOver -= ReduxMethod;
             }
         }
         private float GetDifficultyMultiplier(DifficultyIndex difficultyIndex, bool hasStarstorm2)
